80e881da555261eb122965494433d531b91d6872
hh404358 <122706641+hh404358@users.noreply.github.com>
2024-09-25 11:52:36 +0800

    搭一个vue3的框架

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,11 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from '../components/Header.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
